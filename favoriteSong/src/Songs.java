d33e7a25a6e3255a2bd253f6c0561b19d02dec70
Yano <celiayano24@gmail.com>
2021-08-13 10:44:20 +0300

    Add files via upload

---
+++ favoriteSong/src/Songs.java
@@ -0,0 +1,23 @@
+//name: Monica Ogonjo
+/*The following file contains details of the favorite song and artisr*/
+
+public class Songs {
+    public static void main(String[] args){
+        String song = "Sodade";
+        String artist = "Cesaria Evora";
+        String album = " Miss Perfumado";
+        int yearReleased = 1992;
+        String genre = "Coladiera Cuban Jazz";
+        double duration = 2.32;
+
+        float fnumber = (float) 1.9;
+        char favLetter = 'C';
+
+        System.out.println("My favorite song is "+ song + " Performed by "+ artist +
+                 " released in " + yearReleased + ". It belongs to the "+ genre +
+                " genre. It is " + duration + " mins Long.");
+        System.out.println("Well this is to show that also float "+ fnumber + " and Character "
+                +favLetter + " can also be part of the conversation.");
+
+    }
+}
